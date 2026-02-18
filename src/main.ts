5be2dc9ada8b1af120b584a39eb3b3ba56886a1a
Joel-Lavia <joeldisidi8@gmail.com>
2026-02-18 15:07:00 +0100

    feat:integration swaagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import  dotenv from 'dotenv';
+import dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  
-  
-  await app.listen(process.env.PORT ?? 3000);
+  //===========SWAGGER CONFIG======================
+  const config = new DocumentBuilder()
+    .setTitle('Kin Geek Zone')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('KGZ')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  //=====================================================
+  await app.listen(PORT!, () => console.log(`Port running in PORT : ${PORT}`));
 }
 bootstrap();
